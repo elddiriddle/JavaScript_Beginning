eb5c057bdb0383abaea5c30910d0550994ab23f8
elddiriddle <robertriddle2@gmail.com>
2019-02-24 09:23:46 -0500

    added some functions

---
+++ Functions.js
@@ -0,0 +1,39 @@
+// FUNCTIONS:
+
+// keyword identifier(perameters){ <--open function body with '{'
+//   function body b/t {} - do something = calulation, logic...
+// } <--close function body with }
+function getReminder(){ // declaration of a function is bound to its identifier or name
+    console.log('Feed the dog') // body of the function between { }
+}
+
+//>>>>>>> Calling a Function <<<<<<<<<<<<<<<<<----------------------------------+
+function getTakeOutTheTrash(){
+    console.log('Trash goes out on Tuesday');//<<---- use simicolon at end of line;
+}
+getTakeOutTheTrash(); //<<---- calling the function... identifier()
+
+
+//>>>>>>> Parameters and Arguments <<<<<<<---------------------------------------+
+function sayThanks(name) {
+    console.log('Thank you for your purchase '+ name + '! We appreciate your business.');
+  }
+sayThanks('Alley');
+
+
+//>>>>>>> Default Parameters <<<<<<<----------------------------------------------+
+function greeting (name = 'stranger') {
+    console.log(`Hello, ${name}!`)
+
+
+
+
+
+
+
+
+
+
+
+
+
