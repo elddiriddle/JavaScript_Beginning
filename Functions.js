58a1126dca796dfae6a1d0bb5a8eb9c984217e44
elddiriddle <robertriddle2@gmail.com>
2020-02-15 23:09:13 -0500

    Update Functions.js

--- Functions.js
+++ Functions.js
@@ -25,7 +25,14 @@ sayThanks('Alley');
 function greeting (name = 'stranger') {
     console.log(`Hello, ${name}!`)
 
+greeting('Nick') // Output: Hello, Nick!
+greeting() // Output: Hello, stranger!
 
+function makeShoppingList(item1 ='milk', item2='bread', item3='eggs'){
+ console.log(`Remember to buy ${item1}`);
+ console.log(`Remember to buy ${item2}`);
+ console.log(`Remember to buy ${item3}`);
+}
 
 
 
